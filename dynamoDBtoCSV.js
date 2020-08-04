bbf48181be717e953ad2542d5812034e79afadc1
Peter Wang <msnwangping@hotmail.com>
2020-08-04 22:37:52 +0100

    Update dynamoDBtoCSV.js

--- dynamoDBtoCSV.js
+++ dynamoDBtoCSV.js
@@ -11,6 +11,7 @@ program
   .version("0.0.1")
   .option("-t, --table [tablename]", "Add the table you want to output to csv")
   .option("-d, --describe")
+  .option("-j, --json", "Save output into json files, this will ignore -f")
   .option("-r, --region [regionname]")
   .option(
     "-e, --endpoint [url]",
@@ -24,6 +25,9 @@ program
     "Load AWS Credentials using AWS Credential Provider Chain"
   )
   .option("-s, --size [size]", "Number of lines to read before writing.", 5000)
+  .option("--page-Size [pageSize]", "Pagination, this is page size. We will make a query for each page, page size is also the number of lines to read in each query.", 1)
+  .option("--cooldown-before-query [cooldownBeforeQuery]", "Cooldown time between queries in ms.", 1 *1000)
+  .option("--speed-up-by [speedUpBy]", "To slowly warm up the auto scaling, increase the page size every query by this number.", 1)
   .parse(process.argv);
 
 if (!program.table) {
@@ -77,16 +81,21 @@ var dynamoDB = new AWS.DynamoDB();
 
 var query = {
   TableName: program.table,
-  Limit: 1000
+  Limit: program.pageSize || 1// Pagination, page size
 };
 
 // if there is a target file, open a write stream
-if (!program.describe && program.file) {
+if (!program.describe && program.file && !program.json) {
   var stream = fs.createWriteStream(program.file, { flags: 'a' });
 }
 var rowCount = 0;
 var writeCount = 0;
-writeChunk = program.size;
+let queryCount = 1;
+let fileCount = 0;
+
+let json = {Items:[]};
+
+writeChunk = program.size || 1;
 
 var describeTable = function (query) {
   dynamoDB.describeTable(
@@ -112,7 +121,9 @@ var scanDynamoDB = function (query) {
           // once the designated number of items has been read, write out to stream.
           unparseData(data.LastEvaluatedKey);
         }
-        scanDynamoDB(query);
+        console.log(`query : ${queryCount++}`);
+        query.Limit = query.Limit + program.speedUpBy
+        setTimeout(()=>scanDynamoDB(query), program.cooldownBeforeQuery);
       } else {
         unparseData("File Written");
       }
@@ -121,6 +132,45 @@ var scanDynamoDB = function (query) {
     }
   });
 };
+
+var scanDynamoDB2Json = function (query) {
+  dynamoDB.scan(query, function (err, data) {
+    console.log(`Query No.${queryCount++}`);
+    if (!err) {
+      rowCount = rowCount + data.Items.length;
+      console.log(`loaded ${data.Items.length}, total: ${rowCount}`);
+      json.Items.push(...data.Items); // Print out the subset of results.
+      if (data.LastEvaluatedKey) {
+        // Result is incomplete; there is more to come.
+        query.ExclusiveStartKey = data.LastEvaluatedKey;
+        if (json.Items.length >= writeChunk) {
+          // once the designated number of items has been read, write out to stream.
+          save2JsonFile(json);
+          json.Items = [];
+        }
+        query.Limit = query.Limit + program.speedUpBy;
+        setTimeout(()=>scanDynamoDB2Json(query), program.cooldownBeforeQuery);
+      } else {
+        if(json.Items.length > 0) {
+          save2JsonFile(json);
+        }
+        console.log(`File Written, total count : ${rowCount}`);
+      }
+    } else {
+      console.dir(err);
+    }
+  });
+};
+
+let outputFolder = `ouput/${new Date().toISOString().slice(0,19)}`
+function save2JsonFile(json) {
+  let folderPath = `./${outputFolder}`;
+	if (!fs.existsSync(folderPath)) {
+		fs.mkdirSync(folderPath, { recursive: true});
+  }
+  console.log(`Chunk No.${fileCount}, chunk size: ${writeChunk}`);
+  fs.writeFileSync(`./${outputFolder}/${fileCount++}.json`, JSON.stringify(json, null, 2));
+}
 
 var unparseData = function (lastEvaluatedKey) {
   var endData = Papa.unparse({
@@ -177,4 +227,5 @@ function unMarshalIntoArray(items) {
 }
 
 if (program.describe) describeTable(query);
-else scanDynamoDB(query);
+else if (program.json) scanDynamoDB2Json(query);
+else canDynamoDB(query);
